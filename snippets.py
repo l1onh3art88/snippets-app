ebf855af03dfb64ee74fad2229277a007a890c2f
Spencer Cheng <spenccheng@gmail.com>
2016-07-01 15:18:28 +0000

    solving bugs

--- snippets.py
+++ snippets.py
@@ -17,7 +17,13 @@ def put(name, snippet):
     
     cursor = connection.cursor()
     command = "insert into snippets values (%s, %s)"
-    cursor.execute(command, (name, snippet))
+    try:
+        command = "insert into snippets values (%s, %s)"
+        cursor.execute(command, (name, snippet))
+    except psycopg2.IntegrityError as e:
+        connection.rollback()
+        command = "update snippets set message=%s where keyword=%s"
+        cursor.execute(command, (snippet, name))
     connection.commit()
     logging.debug("Snippet stored successfully.")
     return name, snippet
@@ -33,6 +39,10 @@ def get(name):
     command = "select keyword, message from snippets where keyword = (%s)"
     cursor.execute(command,(name,))
     x = cursor.fetchone()
+    connection.commit()
+    if not x:
+        #No snippet was found
+        return "404: Snippet Not Found"
     return x[1]
     
 def main():
