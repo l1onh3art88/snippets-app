f858d0af0bced2d7d8f725264b0c785a303c5256
Spencer Cheng <spenccheng@gmail.com>
2016-07-01 16:23:41 +0000

    search and catalog function

--- snippets.py
+++ snippets.py
@@ -47,6 +47,32 @@ def get(name):
         return "404: Snippet Not Found"
     return x[0]
     
+def catalog():
+    """
+    Looks up the possible keywords for snippets
+    """
+    
+    cursor = connection.cursor()
+    command = "select keyword from snippets order by keyword"
+    with connection, connection.cursor() as cursor:
+        cursor.execute(command)
+        catalog = cursor.fetchall()
+    logging.debug("Returned all keywords successfully")
+    return catalog
+    
+def search(string):
+    """
+    Looks up snippets that contain the provided string in the message
+    """
+    
+    cursor = connection.cursor()
+    command ="select keyword, message from snippets where message like \'%" + string + "%\'"
+    with connection, connection.cursor() as cursor:
+        cursor.execute(command)
+        results = cursor.fetchall()
+    logging.debug("Found all snippets that contain your string")
+    return results
+    
 def main():
     """Main function"""
     logging.info("Constructing parser")
@@ -60,10 +86,19 @@ def main():
     put_parser.add_argument("snippet", help="Snippet text")
     
     # Subparser for the put command
-    logging.debug("Constructiong get subparser")
+    logging.debug("Constructing get subparser")
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="Name of the snippet")
-
+    
+    #Subparser for the catalog command
+    logging.debug("Constructing catalog subparser")
+    catalog_parser = subparsers.add_parser("catalog", help="Retrieves available keywords")
+    
+    #Subparser for the search command
+    logging.debug("Constructing search subparser")
+    search_parser = subparsers.add_parser("search", help="Looks up snippets containing the provided string")
+    search_parser.add_argument("string", help="String to find message")
+    
 
     arguments = parser.parse_args()
     # Convert parsed arguments from Namespace to dictionary
@@ -76,6 +111,12 @@ def main():
     elif command == "get":
         snippet = get(**arguments)
         print("Retrieved snippet: {!r}".format(snippet))
+    elif command =="catalog":
+        keys = catalog()
+        print("Catalog of keywords: {!r}".format(keys))
+    elif command =="search":
+        results = search(**arguments)
+        print("Your search was matched with these snippets: {!r}".format(results))
 
 if __name__ == "__main__":
     main()
